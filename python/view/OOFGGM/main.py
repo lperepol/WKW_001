4eb32da22540083fd540d894dbb757c5904428f6
lperepol <lawrence.perepolkin@gmail.com>
2022-09-06 00:17:38 +0000

    fixes

--- python/view/OOFGGM/main.py
+++ python/view/OOFGGM/main.py
@@ -5,13 +5,15 @@ import json
 from sortedcontainers import SortedList, SortedSet, SortedDict
 
 def read_metadata():
-    fn = "../Metadata/ManualEdits/KeepMetadata_002.csv"
+    fn = "../../MetaData/AddTaxonomies/MasterMetadata_000.csv"
     df = pd.read_csv(fn)
+    df = df.fillna('Not Specified')
     return df
 
 
 def fixup(df):
-    df = df.fillna(0)
+    return
+    df = df.fillna('')
     for index, row in df.iterrows():
         Magnification = str(row['Magnification']).strip()
         if Magnification.isdigit():
@@ -22,67 +24,44 @@ def fixup(df):
 
 def get_view(adic, df):
     for index, row in df.iterrows():
-        View = str(row['Detail']).strip()
-        if View == '0':
-            continue
-        View = str(row['Detail']).strip()
+        View = str(row['media_descriptor']).strip()
+        View = str(row['media_descriptor']).strip()
         adic[View] = SortedDict()
     return adic
 
 def get_order(adic, df):
     for index, row in df.iterrows():
-        View = str(row['Detail']).strip()
-        if View == '0':
-            continue
+        View = str(row['media_descriptor']).strip()
 
-        Order = str(row['Order']).strip()
+        Order = str(row['order']).strip()
         adic[View][Order] = SortedDict()
     return adic
 
 def get_family(adic, df):
     for index, row in df.iterrows():
-        View = str(row['Detail']).strip()
-        if View == '0':
-            continue
-        Order = str(row['Order']).strip()
-        Family = str(row['Family']).strip()
+        View = str(row['media_descriptor']).strip()
+        Order = str(row['order']).strip()
+        Family = str(row['family']).strip()
         adic[View][Order][Family] = SortedDict()
     return adic
 
 def get_genus(adic, df):
     for index, row in df.iterrows():
-        View = str(row['Detail']).strip()
-        if View == '0':
-            continue
-        Order = str(row['Order']).strip()
-        Family = str(row['Family']).strip()
-        Genus = str(row['Genus']).strip()
+        View = str(row['media_descriptor']).strip()
+        Order = str(row['order']).strip()
+        Family = str(row['family']).strip()
+        Genus = str(row['genus']).strip()
         adic[View][Order][Family][Genus] = SortedDict()
     return adic
 
 def get_gender(adic, df):
     for index, row in df.iterrows():
-        View = str(row['Detail']).strip()
-        if View == '0':
-            continue
-        Order = str(row['Order']).strip()
-        Family = str(row['Family']).strip()
-        Genus = str(row['Genus']).strip()
-        Gender = str(row['Gender']).strip()
-        adic[View][Order][Family][Genus][Gender] = SortedDict()
-    return adic
-
-def get_magnification(adic, df):
-    for index, row in df.iterrows():
-        View = str(row['Detail']).strip()
-        if View == '0':
-            continue
-        Order = str(row['Order']).strip()
-        Family = str(row['Family']).strip()
-        Genus = str(row['Genus']).strip()
-        Gender = str(row['Gender']).strip()
-        Magnification = str(row['Magnification']).strip()
-        adic[View][Order][Family][Genus][Gender][Magnification] = list()
+        View = str(row['media_descriptor']).strip()
+        Order = str(row['order']).strip()
+        Family = str(row['family']).strip()
+        Genus = str(row['genus']).strip()
+        Gender = str(row['sex']).strip()
+        adic[View][Order][Family][Genus][Gender] = list()
     return adic
 
 
@@ -90,26 +69,28 @@ def get_image_file_name(adic, df):
     orderSet = set()
     FamilySet = set()
     GenusSet = set()
-    ImageSet = set()
+    ImageSet = list()
     for index, row in df.iterrows():
-        Order = str(row['Order']).strip()
-        Family = str(row['Family']).strip()
-        Genus = str(row['Genus']).strip()
-        Gender = str(row['Gender']).strip()
-        Magnification = str(row['Magnification']).strip()
-        View = str(row['Detail']).strip()
+        View = str(row['media_descriptor']).strip()
+        Order = str(row['order']).strip()
+        Family = str(row['family']).strip()
+        Genus = str(row['genus']).strip()
+        Gender = str(row['sex']).strip()
         orderSet.add(Order)
         FamilySet.add(Family)
         GenusSet.add(Genus)
 
-        if View == '0':
-            continue
 
-        ImageName = str(row['ImageName']).strip()
-        ImageSet.add(ImageName)
+        ImageName = str(row['image_file']).strip()
+        copyright_institution = str(row['copyright_institution']).strip()
+        photographer = str(row['photographer']).strip()
+        source = str(row['source']).strip()
+        tup = (ImageName,copyright_institution,photographer,source )
 
-        if ImageName not in adic[View][Order][Family][Genus][Gender][Magnification]:
-            adic[View][Order][Family][Genus][Gender][Magnification].append(ImageName)
+        ImageSet.append(ImageName)
+
+        if tup not in adic[View][Order][Family][Genus][Gender]:
+            adic[View][Order][Family][Genus][Gender].append(tup)
 
     print ("Order:" + str(len(orderSet)) + ", Family:" + str(len(FamilySet)) + ", Genus:" + str(len(GenusSet)) + ", Completed:" + str(len(ImageSet)) )
     # Order:12, Family:67, Genus:248
@@ -125,36 +106,27 @@ def get_UNL_stats(df):
     SpeciesSet = set()
     ImageSet = set()
     for index, row in df.iterrows():
-        Order = str(row['Order']).strip()
-        Family = str(row['Family']).strip()
-        Genus = str(row['Genus']).strip()
-        Gender = str(row['Gender']).strip()
-        Magnification = str(row['Magnification']).strip()
-        View = str(row['Detail']).strip()
-        Location = str(row['Location']).strip()
-        Host = str(row['Host']).strip()
-        Species = str(row['Species']).strip()
-        ImageName = str(row['ImageName']).strip()
+        Order = str(row['order']).strip()
+        Family = str(row['family']).strip()
+        Genus = str(row['genus']).strip()
+        Gender = str(row['sex']).strip()
+        View = str(row['media_descriptor']).strip()
+        ImageName = str(row['image_file']).strip()
         orderSet.add(Order)
         FamilySet.add(Family)
-        LocationSet.add(Location)
         ObservationSet.add(View)
-        HostSet.add(Location)
-        GenusSet.add(Host)
-        SpeciesSet.add(Species)
+        GenusSet.add(Genus)
         ImageSet.add(ImageName)
 
     statsDict = dict()
     statsDict["Orders"] = len(orderSet)
     statsDict['Families'] = len(FamilySet)
     statsDict['Genera'] = len(GenusSet)
-    statsDict['Species'] = len(SpeciesSet)
-    statsDict['Locations'] = len(LocationSet)
     statsDict['Observations'] = len(ObservationSet)
     statsDict['Micrographs'] = len(ImageSet)
 
     json_object = json.dumps(statsDict, indent=4)
-    fn = "../../../files/json/unl/Unl_stats.json"
+    fn = "stats.json"
 
     with open(fn, "w") as outfile:
         json.dump(statsDict, outfile,indent=4)
@@ -163,7 +135,7 @@ def get_UNL_stats(df):
 
 def writeDict2Json(adict):
     json_object = json.dumps(adict, indent=4)
-    fn = "../../../files/json/unl/OOFGGM.json"
+    fn = "OOFGGM.json"
 
     with open(fn, "w") as outfile:
         json.dump(adict, outfile,indent=4)
@@ -171,14 +143,13 @@ def writeDict2Json(adict):
 
 def main():
     df = read_metadata()
-    df = fixup(df)
+    #df = fixup(df)
     nematode_dict = SortedDict()
     nematode_dict = get_view(nematode_dict, df)
     nematode_dict = get_order(nematode_dict, df)
     nematode_dict = get_family(nematode_dict, df)
     nematode_dict = get_genus(nematode_dict, df)
     nematode_dict = get_gender(nematode_dict, df)
-    nematode_dict = get_magnification(nematode_dict, df)
     nematode_dict = get_image_file_name(nematode_dict, df)
     jo = writeDict2Json(nematode_dict)
     get_UNL_stats(df)
